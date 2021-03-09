ce37784b4dfbe3f6a989700dfacaa72398c76959
= <=>
2021-03-09 22:08:39 +0100

    0.0.3

--- projects/business-registration-stepper-example/src/app/app.module.ts
+++ projects/business-registration-stepper-example/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BusinessRegistrationStepperModule } from 'business-registration-stepper';
 import { AgmCoreModule } from '@agm/core';
